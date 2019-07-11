02960aa1e3f8f0344e88c0a6f19cf7da4adfba1e
大大狐狸 <hitfzyangdianshi@hotmail.com>
2019-07-11 18:58:04 +0800

    by CLion

---
+++ cmake-build-debug/CMakeFiles/mymasterproject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mymasterproject.dir/mymasterproject.cpp.o"
+  "CMakeFiles/mymasterproject.dir/static_model.c.o"
+  "mymasterproject.pdb"
+  "mymasterproject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/mymasterproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
